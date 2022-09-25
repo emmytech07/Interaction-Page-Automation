9ab735f76974b041ad91f5b4cf0e38315e34b8ee
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-25 02:30:15 +0100

    Actions

--- app.py
+++ app.py
@@ -16,3 +16,6 @@ driver.maximize_window()
 driver.get("https://rahulshettyacademy.com/AutomationPractice/")
 action = ActionChains(driver)
 
+action.move_to_element(driver.find_element(By.ID, 'mousehover')).perform()
+action.context_click(driver.find_element(By.LINK_TEXT, 'Top')).perform()
+action.move_to_element(driver.find_element(By.LINK_TEXT, 'Reload')).click().perform()
