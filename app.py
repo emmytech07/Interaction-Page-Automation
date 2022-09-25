4c1fc688eb75a97c8eee9fad096e5eb76ccc7a24
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-25 02:21:33 +0100

    implementation

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from selenium import webdriver
 
+from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time as sl
@@ -13,3 +14,5 @@ driver.implicitly_wait(5)
 
 driver.maximize_window()
 driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+action = ActionChains(driver)
+
